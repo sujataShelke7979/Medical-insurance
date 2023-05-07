e4e1cf57e06d6dda4c3247c16d5582f4af04e1d1
Ravindra Thutte <sujatathutte@7979gmail.com>
2023-05-07 17:43:05 +0530

    main file

---
+++ main.py
@@ -0,0 +1,49 @@
+from flask import Flask, jsonify, render_template, request
+from project_app.utils import MedicalInsurance
+
+# Creating instance here
+# 'app' is standard variable
+app = Flask(__name__)
+
+# @app.route("/") --> USED TO GET HOME API
+# @app.route("/furniture") --> You will get 'Furniture' Page here
+
+@app.route("/")
+def hello_flask():
+    print("Welcome to Medical Insurance company")
+    return render_template("index.html")
+
+
+@app.route("/predict_charges",methods=["POST","GET"])
+def get_insurance_charges():
+    if request.method =="GET":
+        print("We are in a GET Method")
+        #data=request.form
+        #print("Data--->\n",data)
+        # age=eval(data["age"])
+        # sex=data["sex"]
+        # bmi=eval(data["bmi"])
+        # children=eval(data["children"])
+        # smoker=data["smoker"]
+        # region=data["region"]
+
+
+        age=int(request.args.get("age"))
+        sex=(request.args.get("sex"))
+        bmi=float(request.args.get("bmi"))
+        children=int(request.args.get("children"))
+        smoker=request.args.get("smoker")
+        region=request.args.get("region")
+
+    
+    
+    
+    
+    med_ins=MedicalInsurance(age,sex,bmi,children,smoker,region)
+    charges=med_ins.get_predicted_charges()
+    return render_template("index.html",prediction=charges)
+print("__name__-->",__name__)
+if __name__=="__main__":
+    #app.run(host="0.0.0.0",post=5000,debug=false)
+    app.run()
+
